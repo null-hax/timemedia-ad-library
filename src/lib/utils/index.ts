79597f1d65538212653a4a7bac439c318fd2cc86
hax <admin@nullhax.com>
2024-12-02 09:46:38 -0800

    initial shadcn components and basic setup

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString()
+}
+
+export function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+} 
