e4f95bcfc0c1d7d131208d25dbb7c1421cc4f327
hax <admin@nullhax.com>
2024-12-03 16:46:12 -0800

    improve loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import dynamic from 'next/dynamic'
+import { Hero } from '@/components/Hero'
 
 // Dynamically import with proper config
 const AdsLibraryClient = dynamic(
   () => import('@/components/AdsLibraryClient').then(mod => mod.default),
   {
-    loading: () => <div>Loading...</div>
+    loading: () => <Hero />
   }
 )
 
