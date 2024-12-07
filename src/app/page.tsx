0582ffe8ef10df4ee0a7efff95079ff9f529e1ff
hax <admin@nullhax.com>
2024-12-07 14:13:22 -0800

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from '@/components/Hero'
 import { AdsGrid } from '@/components/AdsGrid'
 import { getAds } from '@/lib/services/ads'
-import { USE_SUPABASE } from '@/lib/config'
 
 export default async function Page() {
   // Pre-fetch the ads data
