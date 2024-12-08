a2974ffb1c531bee88728599f5f3204fb4e99207
hax <admin@nullhax.com>
2024-12-07 16:38:20 -0800

    temp

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -30,7 +30,7 @@ export function CardView({
     )
   }
 
-  if (loading) {
+  if (loading || !loading) {
     return (
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 9 }).map((_, i) => (
